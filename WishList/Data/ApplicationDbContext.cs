9c533a218ef294cdaf3f9ccdca85bac34ae4d108
Cleyton José <c.rodrigues.da.silva@avanade.com>
2020-04-04 21:55:47 -0300

    Correction to tasks.

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -10,8 +10,7 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-
-        public DbSet<Item> Items; 
+        public DbSet<Item> Items;
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
     }
